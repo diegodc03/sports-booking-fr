b02899bc8e3e766350d5b451e592d784459fe8d4
DIEGO DE CASTRO MERILLAS <diegodecastro@usal.es>
2025-10-26 11:06:14 +0100

    add klarna config

---
+++ karma.conf.js
@@ -0,0 +1,52 @@
+// karma.conf.js
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false // deja visible el resultado en navegador (solo local)
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }]
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-gpu',
+          '--disable-dev-shm-usage',
+          '--headless',
+          '--remote-debugging-port=9222'
+        ]
+      }
+    },
+    singleRun: false,
+    restartOnFileChange: true
+  });
+
+  // 🧠 Si está corriendo en CI (GitHub Actions, GitLab, etc.)
+  if (process.env.CI) {
+    config.set({
+      browsers: ['ChromeHeadlessCI'],
+      singleRun: true,
+      autoWatch: false,
+    });
+  }
+};
