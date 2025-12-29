5a42a62f467fb395bfa775128649387ac265756f
Diego de Castro Merillas <116064701+diegodc03@users.noreply.github.com>
2025-12-29 20:06:28 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 
 export const environment = {
-  baseUrl: 'http://localhost:8080/api/v1',
+  baseUrl: 'http://192.168.1.172:8080/api/v1',
   booking: '/booking',
   results: '/results',
   adminPanel: '/adminPanel',
